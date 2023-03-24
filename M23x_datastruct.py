ddc72b2c08a76be99f01bc4787bd7fbc834ff2aa
Merholg <ixibitus@gmail.com>
2023-03-24 17:20:03 +0300

    Add class B1B2B3B4B5B6 2.3.16 Чтение варианта исполнения. PRODUCTIONVAR

--- M23x_datastruct.py
+++ M23x_datastruct.py
@@ -42,13 +42,178 @@ class ByteX2X6(ctypes.Union):
         _fields_ = [
             ("direct_act", c_uint8, 1),
             ("direct_react", c_uint8, 1),
-            ("b1", c_uint8, 6),
+            ("b1", c_uint8, 6)
         ]
 
     _fields_ = [
         ("x2x6", X2X6),
         ("one_byte", c_uint8)
     ]
+
+
+class B1B2B3B4B5B6:
+    """
+        //2.3.16 Чтение варианта исполнения. PRODUCTIONVAR
+        //Поле данных ответа состоит из 6 байт
+        struct PRODUCTIONVAR
+    {
+    //---------------------------------
+    //---------------------------------
+    //---------------------------------
+    //---------------------------------
+    //---------------------------------
+    };
+    """
+
+    class ByteB1X2222(ctypes.Union):
+        class B1X2222(ctypes.BigEndianStructure):
+            """
+            BYTE In:2;             // Iн Iн - номинальный ток А: 0 - 5; 1 - 1; 2 - 10.
+    	    BYTE Un:2;             // Uн Uн - номинальное напряжение В: 0 - 57,7; 1 - 230.
+    	    BYTE ClR:2;            // Cl R класс точности по реактивной энергии %: 0 - 0,2; 1 - 0,5; 2 - 1,0; 3 - 2,0.
+    	    BYTE ClA:2;            // Cl А класс точности по активной энергии %: 0 - 0,2; 1 - 0,5; 2 - 1,0; 3 - 2,0.
+            """
+            _pack_ = 1
+            _fields_ = [
+                ("In", c_uint8, 2),
+                ("Un", c_uint8, 2),
+                ("ClR", c_uint8, 2),
+                ("ClA", c_uint8, 2)
+            ]
+
+        _fields_ = [
+            ("b1x2222", B1X2222),
+            ("one_byte", c_uint8)
+        ]
+
+    class ByteB2X41111(ctypes.Union):
+        class B2X41111(ctypes.BigEndianStructure):
+            """
+            BYTE MeterConst:4;     // Постоянная счетчика имп/квт?ч 0 - 5000; 1 - 25000; 2 - 1250; 3 - 500; 4 - 1000; 5 - 250.
+	        BYTE NPhase:1;         // Число фаз 0 - 3, 1 - 1
+	        BYTE ProfMPower:1;     // Учет профиля средних мощностей 0 - нет, 1 - да
+	        BYTE TempRange:1;      // Температурный диапазон°C 0 – 20, 1 – 40
+	        BYTE NDirect:1;        // Число направлений 0 - 2, 1 - 1
+            """
+            _pack_ = 1
+            _fields_ = [
+                ("MeterConst", c_uint8, 4),
+                ("NPhase", c_uint8, 1),
+                ("ProfMPower", c_uint8, 1),
+                ("TempRange", c_uint8, 1),
+                ("NDirect", c_uint8, 1)
+            ]
+
+        _fields_ = [
+            ("b2x41111", B2X41111),
+            ("one_byte", c_uint8)
+        ]
+
+    class ByteB3X4211(ctypes.Union):
+        class B3X4211(ctypes.BigEndianStructure):
+            """
+            BYTE NVarProd:4;       // No варианта исполнения 1 - 57,7В(1)5А10А5000имп./кВт*ч 2 - 230В5А60А500имп./кВт*ч 3 - 230В5А100А250имп./кВт*ч 4 - 230В(1)5А10А1000имп./кВт*ч
+	        BYTE MeterType:2;      // Тип счетчика 0 - AR, 1 - A
+	        BYTE Tarificator:1;    // Тарификатор 0 - внешний, 1 - внутренний
+	        BYTE SumPhase:1;       // Суммирование фаз 0 - с учетом знака, 1 - по модулю
+            """
+            _pack_ = 1
+            _fields_ = [
+                ("NVarProd", c_uint8, 4),
+                ("MeterType", c_uint8, 2),
+                ("Tarificator", c_uint8, 1),
+                ("SumPhase", c_uint8, 1)
+            ]
+
+        _fields_ = [
+            ("x2x2x2x2", B3X4211),
+            ("one_byte", c_uint8)
+        ]
+
+    class ByteB4X1121111(ctypes.Union):
+        class B4X1121111(ctypes.BigEndianStructure):
+            """
+            BYTE EPlonb:1;         // Эл. помба верхней крышки 0 - нет, 1 - есть
+	        BYTE ExSupp:1;         // Внешнее питание 0 - нет, 1 - есть
+	        BYTE IFace:2;          // Интерфейс 0 - CAN, 1 - RS-485, 2 - резерв, 3 - нет
+	        BYTE OPort:1;          // оптопорт 0 - нет, 1 - есть
+	        BYTE ModemGSM:1;       // Модем GSM 0 - нет, 1 - есть
+	        BYTE ModemPLM:1;       // Модем PLM 0 - нет, 1 - есть
+	        BYTE Mem3:1;           // Память No3 0 - 65.5x8, 1 - 131x8
+            """
+            _pack_ = 1
+            _fields_ = [
+                ("EPlonb", c_uint8, 1),
+                ("ExSupp", c_uint8, 1),
+                ("IFace", c_uint8, 2),
+                ("OPort", c_uint8, 1),
+                ("ModemGSM", c_uint8, 1),
+                ("ModemPLM", c_uint8, 1),
+                ("Mem3", c_uint8, 1)
+            ]
+
+        _fields_ = [
+            ("b4x1121111", B4X1121111),
+            ("one_byte", c_uint8)
+        ]
+
+    class ByteB5X11111111(ctypes.Union):
+        class B5X11111111(ctypes.BigEndianStructure):
+            """
+	        BYTE PhCalcPower:1;    // Пофазный учет энергии A+ 0 - нет, 1 - да
+	        BYTE QPower:1;         // Контроль ПКЭ 0 - нет, 1 - да
+	        BYTE SupIF1:1;         // Встроенное питание интерфейса 1 0 - нет, 1 - да
+	        BYTE IFace2:1;         // Интерфейс 2 0 - нет, 1 - да
+	        BYTE CEPlomb:1;        // Флаг наличия эл. пломбы защитной крышки, 0 - нет, 1 - есть
+	        BYTE TarMax:1;         // Флаг потарифного учета максимумов мощности, 0 - нет, 1 - есть
+	        BYTE Light:1;          // Флаг наличия подсветки ЖКИ, 0 - нет, 1 - есть
+	        BYTE Relay:1;          // Флаг наличия встроенного реле, 0 - нет, 1 - есть
+            """
+            _pack_ = 1
+            _fields_ = [
+                ("PhCalcPower", c_uint8, 1),
+                ("QPower", c_uint8, 1),
+                ("SupIF1", c_uint8, 1),
+                ("IFace2", c_uint8, 1)
+                ("CEPlomb", c_uint8, 1),
+                ("TarMax", c_uint8, 1),
+                ("Light", c_uint8, 1),
+                ("Relay", c_uint8, 1)
+            ]
+
+        _fields_ = [
+            ("b5x11111111", B5X11111111),
+            ("one_byte", c_uint8)
+        ]
+
+    class ByteB6X11111111(ctypes.Union):
+        class B6X11111111(ctypes.BigEndianStructure):
+            """
+	        BYTE ExControl:1;      // Флаг наличия аппаратных средств управления внешними устройствами отключения нагрузки, 0 - нет, 1 - есть
+	        BYTE VoltTarif:1;      // Флаг переключения тарифов внешним напряжением, 0 - нет, 1 - да
+	        BYTE BEPlomb:1;        // Флаг наличия эл.пломбы модульного отсека, 0 - нет, 1 - есть
+	        BYTE Profile2:1;       // Флаг наличия профиля 2 0 - нет, 1 - есть
+	        BYTE ModemPLC2:1;      // Модем PLC2, 0 - нет, 1 - есть
+	        BYTE IEC61107:1;       // Флаг протокола IEC61107, 0 - нет, 1 - да
+	        BYTE Reserved1:1;      // Reserved
+	        BYTE Reserved2:1;      // Reserved
+            """
+            _pack_ = 1
+            _fields_ = [
+                ("ExControl", c_uint8, 1),
+                ("VoltTarif", c_uint8, 1),
+                ("BEPlomb", c_uint8, 1),
+                ("Profile2", c_uint8, 1)
+                ("ModemPLC2", c_uint8, 1),
+                ("IEC61107", c_uint8, 1),
+                ("Reserved1", c_uint8, 1),
+                ("Reserved2", c_uint8, 1)
+            ]
+
+        _fields_ = [
+            ("b6x11111111", B6X11111111),
+            ("one_byte", c_uint8)
+        ]
 
 
 class B1B2:
@@ -254,7 +419,7 @@ def answer_081111h(in_bytearray):
     m = 3  # общая длина последовательности
     k = 3  # длина последовательности по каждой фазе
     trust_bytearray = in_bytearray[:] if isinstance(in_bytearray, bytearray) and (
-                len(in_bytearray) == m) else bytearray([0] * m)
+            len(in_bytearray) == m) else bytearray([0] * m)
 
     for i in range(0, m, k):
         power = B1B3B2(trust_bytearray[i:i + k])
@@ -286,7 +451,7 @@ def answer_081408h(in_bytearray):
     m = 16  # общая длина последовательности
     k = 4  # длина последовательности по каждой фазе
     trust_bytearray = in_bytearray[:] if isinstance(in_bytearray, bytearray) and (
-                len(in_bytearray) == m) else bytearray([0] * m)
+            len(in_bytearray) == m) else bytearray([0] * m)
 
     for i in range(0, m, k):
         power = B2B1x2x6B4B3(trust_bytearray[i:i + k])
